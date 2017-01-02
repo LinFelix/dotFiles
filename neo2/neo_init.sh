fb1c3914d83de10b3d5ee0b5e99219d2c39d6a77
LinFelix <felix-voelker@gmx.net>
2017-01-02 14:21:00 +0100

    add script to set xkeyboard to neo2

---
+++ neo2/neo_init.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/zsh
+
+# this command start neo as the first keyboard layout with
+# correct 4th and 6th layers and the normal german keyboard
+# as second layers
+
+#setxkbmap -layout de,de -variant neo,basic
+
+#to only set neo
+setxkbmap -layout de -variant neo
